bd49df6ae982e68f69819a494f69374494a1f981
Choi Junyoung <juny0955@gmail.com>
2026-01-25 00:40:50 +0900

    init

---
+++ src/main/java/junyoung/dev/rlogserver/RlogServerApplication.java
@@ -0,0 +1,13 @@
+package junyoung.dev.rlogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlogServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RlogServerApplication.class, args);
+	}
+
+}
