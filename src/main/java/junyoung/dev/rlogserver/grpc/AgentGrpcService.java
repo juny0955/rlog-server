73ee5f57885be3f48d07ddbc734510ba1644ca8f
Choi Junyoung <juny0955@gmail.com>
2026-01-25 11:24:31 +0900

    grpc 설정

---
+++ src/main/java/junyoung/dev/rlogserver/grpc/AgentGrpcService.java
@@ -0,0 +1,26 @@
+package junyoung.dev.rlogserver.grpc;
+
+import org.springframework.grpc.server.service.GrpcService;
+
+import io.grpc.stub.StreamObserver;
+import junyoung.dev.rlogserver.proto.AgentServiceGrpc;
+import junyoung.dev.rlogserver.proto.LogBatch;
+import junyoung.dev.rlogserver.proto.SendAck;
+
+@GrpcService
+public class AgentGrpcService extends AgentServiceGrpc.AgentServiceImplBase {
+
+	@Override
+	public void send(LogBatch request, StreamObserver<SendAck> responseObserver) {
+
+		SendAck ack = SendAck.newBuilder()
+			.setOk(true)
+			.setMessage("accepted")
+			.setBatchId(request.getBatchId())
+			.setAccepted(request.getLogsCount())
+			.build();
+
+		responseObserver.onNext(ack);
+		responseObserver.onCompleted();
+	}
+}
